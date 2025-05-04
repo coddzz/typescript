b8d297d609b03787bfba9b44a45a6b0b235266f1
Sarath <learn.cypher@gmail.com>
2025-05-04 14:18:46 +0530

    static typing

---
+++ class02/dest/main.js
@@ -0,0 +1,13 @@
+"use strict";
+// Primitive Types
+// number, bool and string
+let empName = "John";
+let employeeName;
+employeeName = "Kevin";
+let isPermanent = true;
+let age = 30;
+// static typing!!
+let multiply = (num) => {
+    return 10 * num;
+};
+console.log(multiply(12));
---
+++ class02/src/main.ts
@@ -0,0 +1,18 @@
+// Primitive Types
+// number, bool and string
+
+let empName = "John";
+let employeeName:string;
+
+employeeName = "Kevin";
+
+let isPermanent:boolean = true;
+
+let age = 30;
+
+// static typing!!
+let multiply = (num:number) =>{
+    return 10 * num;
+}
+
+console.log(multiply(12));
